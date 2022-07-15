c59386585f52cc86d1f994cfe9d911f4fab4d83e
“Brandon <Brandoon@pm.me>
2022-07-15 10:35:00 -0700

    Re-factoring and copy pasted from my other project

---
+++ y0utube_wallpaper.py
@@ -0,0 +1,57 @@
+
+import argparse as ap
+
+from project_func import clear_screen, savePath, show_splash
+
+# Youtube Offline Downloader
+
+
+def main():
+    # get arguments from user using argparse module for only the url
+    parser = ap.ArgumentParser(description="Youtube Offline Downloader")
+    parser.add_argument(
+        "-u", "--url", help="URL of the video or playlist to download", required=False
+    )
+    parser.add_argument(
+        "-p", "--path", help="Path to save the video or playlist", required=False
+    )
+    # playlist argument
+    parser.add_argument(
+        "-pl", "--playlist", help="URL of the playlist to download", required=False
+    )
+
+    # if the url arg is provided, then download the video
+    if parser.parse_args().url:
+        choice1(parser.parse_args().url)
+
+    # if the path arg is provided, then set the path
+    if parser.parse_args().path:
+        savePath(parser.parse_args().path)
+
+    # if the playlist arg is provided, then download the playlist
+    if parser.parse_args().playlist:
+        choice3(parser.parse_args().playlist)
+
+    # if no arguments are passed, show the splash screen
+    clear_screen()
+    show_splash()
+    print("1. Download video")
+    print("2. Default Download Location")
+    print("3. Download Playlist")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        choice1()
+    elif choice == "2":
+        savePath()
+        main()
+    elif choice == "3":
+        choice3()
+    elif choice == "4":
+        print("Exiting...")
+        exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ yt_functions.py
@@ -0,0 +1,112 @@
+import os
+import re  # regex
+from time import sleep
+
+import youtube_dl
+from pyfiglet import Figlet
+
+
+def show_splash():
+    """
+    Display splash screen
+    """
+    clear_screen()
+    title = "Y0utube Wallpaper"
+    f = Figlet(font="standard")
+    print(red(f.renderText(title)))
+
+
+def clear_screen():
+    """
+    It prints 25 new lines
+    """
+    print("\n" * 25)
+
+
+def check_url(url: str):
+    """
+    It takes a string as an argument, checks if it's a valid youtube url, and returns the url if it is,
+    or None if it isn't
+    
+    :param url: The URL of the video to be downloaded
+    :type url: str
+    :return: The match object or None
+    """
+    regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/.+$"
+
+    match = re.match(regex, url)
+    if match:
+        return match.group()
+    elif match is None:
+        return None
+
+
+def green(text: str) -> str:
+    """
+    `green` takes a string and returns a string
+    
+    :param text: the text to be colored
+    :type text: str
+    :return: The text in green.
+    """
+    return "\033[32m" + text + "\033[0m"
+
+
+def red(text: str) -> str:
+    """
+    `red` takes a string and returns a string
+    
+    :param text: The text to be colored
+    :type text: str
+    :return: The text is being returned with the color red.
+    """
+    return "\033[31m" + text + "\033[0m"
+
+
+def blue(text: str) -> str:
+    """
+    `blue` takes a string and returns a string
+    
+    :param text: The text to be colored
+    :type text: str
+    :return: The text is being returned with the color blue.
+    """
+    return "\033[34m" + text + "\033[0m"
+
+
+def download_video(url: str, save_path: str):
+    """
+    Download the video from the given url and save it to the given path
+    
+    :param url: the url of the video you want to download
+    :type url: str
+    :param save_path: The path where you want to save the video
+    :type save_path: str
+    """
+    ydl_opts = {
+        # hight quality video
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio",
+        # save location of the video
+        "outtmpl": save_path + "/%(title)s.%(ext)s",
+    }
+    youtube_dl.YoutubeDL(ydl_opts).extract_info(url)
+
+
+def download_playlist(url: str, save_path: str):
+    """
+    Download playlist from youtube using youtube-dl
+    
+    :param url: the url of the playlist
+    :type url: str
+    :param save_path: The path where you want to save the downloaded videos
+    :type save_path: str
+    """
+    # download playlist from youtube using youtube-dl
+    ydl_opts = {
+        # hight quality video
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio",
+        # save location of the video
+        "outtmpl": save_path + "/%(title)s.%(ext)s",
+        "yes-playlist": True,
+    }
+    youtube_dl.YoutubeDL(ydl_opts).extract_info(url)
