b9eacccdd88ecda397dc2c2fb0b4c33997657c13
“Brandon <Brandoon@pm.me>
2022-07-18 19:28:13 -0700

    2 tests added

---
+++ test_app.py
@@ -0,0 +1,29 @@
+from functions import create_temp_folder, delete_temp_folder, download_video
+import os
+
+temp_path = os.path.expanduser("~") + "/Library/Caches/Y0utube"
+final_path = (
+    os.path.expanduser("~")
+    + "/Library/Containers/whbalzac.Dongtaizhuomian/Data/Documents/Videos"
+)
+
+
+def test_create_temp_folder():
+    """
+    Test the create_temp_folder function
+    """
+    delete_temp_folder()
+    create_temp_folder()
+    assert os.path.exists(temp_path)
+    delete_temp_folder()
+
+
+def test_delete_temp_folder():
+    """
+    Test the delete_temp_folder function
+    """
+    create_temp_folder()
+    delete_temp_folder()
+    assert not os.path.exists(temp_path)
+    delete_temp_folder()
+
